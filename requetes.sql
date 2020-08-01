6c32e6b2f703da09d869532a152d2e39aeb7c304
Honomatopic <10617191+Honomatopic@users.noreply.github.com>
2020-08-01 23:32:12 +0200

    Refactoring projet
    Refactoring projet

--- requetes.sql
+++ requetes.sql
@@ -10,6 +10,18 @@ JOIN inscrire ON session.id = inscrire.session_id
 JOIN employe ON inscrire.employe_id = employe.id
 WHERE employe.id = 22 
 
+-- Affichage des sessions de formations non inscrite de l'employé (par exemple ayant l'id 22) -- 
+SELECT session.id, formation.intitule, duree.datedebut, duree.datefin, salle.nom, intervenant.nom, intervenant.prenom, prestataire.nom 
+FROM session 
+JOIN formation ON session.formation_id = formation.id
+JOIN duree ON session.duree_id = duree.id
+JOIN salle ON session.salle_id = salle.id 
+JOIN intervenant ON session.intervenant_id = intervenant.id
+JOIN prestataire ON session.prestataire_id = prestataire.id
+JOIN inscrire ON session.id = inscrire.session_id 
+JOIN employe ON inscrire.employe_id = employe.id
+WHERE employe.id =! 22 
+
 -- Affichage de la session (par exemple ayant l'id 1) -- 
 SELECT session.id, formation.intitule, duree.datedebut, duree.datefin, salle.nom, intervenant.nom, intervenant.prenom, prestataire.nom
 FROM session  
