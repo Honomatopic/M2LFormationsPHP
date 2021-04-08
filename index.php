64aae44246c61e03e4773c02a8b8059537293cf5
Honoré Rasamoelina <10617191+Honomatopic@users.noreply.github.com>
2021-04-08 20:06:34 +0200

    Refactoring projet avec GNU/Linux (Ubuntu)
    Refactoring projet avec GNU/Linux (Ubuntu)

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ include_once ("_entete.inc.php");
 ?>
 
 
-<h1>Bienvenue sur le site M2L</h1>
+<h1>Bienvenue sur le site de la M2L</h1>
 
 <fieldset>
-	<legend>Pour vous connecter</legend>
+	<legend>Veuillez-vous connecter</legend>
 	<form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
 		<input type="email" name="email" placeholder="Votre email"> <br> <input
 			type="password" name="motpasse" placeholder="Votre mot de passe"> <br>
@@ -15,6 +15,8 @@ include_once ("_entete.inc.php");
 	</form>
 </fieldset>
 </section>
+<br>
+<a href="creerLEmploye.php">&#128395;&#65039; Pour s'inscrire</a>
 <?php
 include_once ("_piedpage.inc.php");
 ?>
