04ad3e8d45b6611c9f7280cad136765b23d79e5f
Honoré Rasamoelina <10617191+Honomatopic@users.noreply.github.com>
2021-07-04 00:20:02 +0200

    Refactoring projet avec PostGreSQL
    Refactoring projet avec PostGreSQL

--- consulterMaSession.php
+++ consulterMaSession.php
@@ -4,7 +4,7 @@ include_once ("_entete.inc.php");
 
 <?php 
 $laSession = consulterLesInformationsDeLaSessionParLId($_GET["id"]);
-echo "<p>".$laSession["id"]."</p>";
+echo "<p>".$laSession["id_session"]."</p>";
 echo "<p>".$laSession["intitule_formation"]."</p>";
 echo "<p>".date("d/m/Y", strtotime($laSession["datedebut"]))."</p>";
 echo "<p>".date("d/m/Y", strtotime($laSession["datefin"]))."</p>";
@@ -13,8 +13,8 @@ echo "<p>".$laSession["nom_intervenant"]."</p>";
 echo "<p>".$laSession["nom_prestataire"]."</p>";
 echo "<td></td>";
 ;?>
-<a href="seDesinscrireDeMaSession.traitement.php?eemploye_id=<?php echo $_SESSION["id"] ;?>&ssession_id=<?php echo $laSession["id"];?>"><button>&#128395;&#65039; Me désinscrire de cette formation</button></a>
-<a href="genererLaSessionEnPdf.php?id=<?php echo $laSession["id"];?>"><button>&#128195; Génerer le PDF</button></a>
+<a href="seDesinscrireDeMaSession.traitement.php?eemploye_id=<?php echo $_SESSION["id"] ;?>&ssession_id=<?php echo $laSession["id_session"];?>"><button>&#128395;&#65039; Me désinscrire de cette formation</button></a>
+<a href="genererLaSessionEnPdf.php?id=<?php echo $laSession["id_session"];?>"><button>&#128195; Génerer le PDF</button></a>
 <?php
 include_once ("_piedpage.inc.php");
 
